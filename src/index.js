d88e4136c35741d3abdcfcae976a7ebb4601e520
niveditha0512 <disisa.t.nive@gmail.com>
2022-07-04 20:13:56 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const date = Date();
+var currentTime = new Date().getHours();
+var morningValue, eveningValue, nightValue;
 
-const date= Date();
-const currentDate = new Date.getFullYear();
-
-
-
+function greetings(time) {
+  if (time === 0 && time < 12) {
+    morningValue = 'Good Morning';
+  }
+  elseif(time >= 12 && time <= 18);
+  {
+    eveningValue = 'Good Evening';
+  }
+  else{
+    nightValue = 'Good night';
+  }
+}
 
 ReactDOM.render(
-<div>
+  <div>
+    <h1>{console.log(greetings(currentTime))}</h1>
+  </div>,
 
-<h1>{date}{}</h1>
-
-  </div>
-
-
-
-
-
-
-  ,document.getElementById("root")
-)
+  document.getElementById('root')
+);
