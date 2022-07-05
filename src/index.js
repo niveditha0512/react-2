97d25db717be6ca542c926158fa8c6f9d89e9434
niveditha0512 <disisa.t.nive@gmail.com>
2022-07-05 19:18:33 +0530

    2 files modified

--- src/index.js
+++ src/index.js
@@ -2,24 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
+
 const date = Date(),
  var value;
 var currentTime = new Date().getHours();
+
 const styling={
   color:""
 }
-
 function greetings(x) {
 
   if (x===0 || x<=12){
 
   
-  <h1> {value="Good Morning !", styling.color='red'
+  <h1> {value="Good Morning !",styling.color="red"
   }</h1>
   }
 else if (x>12 && x<=18) {
 
-<h1 style="color : red;">{value="Good Evening !"}</h1>
+<h1 style={{color:"red"}}>{value="Good Evening !"}</h1>
 
 }
  else {
