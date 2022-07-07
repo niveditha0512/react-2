1b9572e5242e7f0a34d7073dc67a77cf614900ed
niveditha0512 <disisa.t.nive@gmail.com>
2022-07-07 14:27:42 +0530

    2 files modified

--- src/index.js
+++ src/index.js
@@ -7,20 +7,18 @@ const date = Date(),
  var value;
 var currentTime = new Date().getHours();
 
-const styling={
-  color:""
-}
+
 function greetings(x) {
 
   if (x===0 || x<=12){
 
   
-  <h1> {value="Good Morning !",styling.color="red"
+  <h1> {value="Good Morning !"
   }</h1>
   }
 else if (x>12 && x<=18) {
 
-<h1 style={{color:"red"}}>{value="Good Evening !"}</h1>
+<h1>{value="Good Evening !"}</h1>
 
 }
  else {
@@ -31,24 +29,18 @@ else if (x>12 && x<=18) {
 return value;
 
 }
-/*
+
 const Header = () => {
   return (
   
       <div>
-    <h1>{greetings(3)}</h1>
+    <h1>{greetings(currentTime)}</h1>
   </div>
     
   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Header />);*/
-
-ReactDOM.render(<div>
-  <h1>{greetings(3)}</h1>
-</div>,document.getElementById("root")
-  
-)
+root.render(<Header />);
 
 
